c708bbc222a8608ab18141a4f3ceacab205bc8a5
Scott S <scotts@iqmetrix.com>
2018-08-30 11:46:15 -0700

    Working cluster (requires /etc/hosts config)

--- mongosetup/setup.sh
+++ mongosetup/setup.sh
@@ -1,43 +1,27 @@
 #! /bin/bash
 
-MONGODB1=`ping -c 1 mongo1 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
-MONGODB2=`ping -c 1 mongo2 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
-MONGODB3=`ping -c 1 mongo3 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
-
-echo "1" ${MONGODB1}
-echo "2" ${MONGODB2}
-echo "3" ${MONGODB3}
-
 echo "Pausing for a bit"
 sleep 5
-echo "Here we go......"
-# echo "Waiting for startup..."
-# until curl http://mongo1:28017/serverStatus\?text\=1 2>&1 | grep uptime | head -1; do
-#     printf '.'
-#     sleep 5
-# done
+echo "Setting up mongo replicaSet now....."
 
-# echo curl http://mongo1:28017/serverStatus\?text\=1 2>&1 | grep uptime | head -1;
-# echo "Started.."
-
-mongo --host ${MONGODB1}:27017 <<EOF
+mongo --host mongo1:27020 <<EOF
     var cfg = {
         "_id": "rs",
         "version": 1,
         "members": [
             {
                 "_id": 0,
-                "host": "${MONGODB1}:27017",
+                "host": "mongo1:27020",
                 "priority": 2
             },
             {
                 "_id": 1,
-                "host": "${MONGODB2}:27017",
+                "host": "mongo2:27021",
                 "priority": 0
             },
             {
                 "_id": 2,
-                "host": "${MONGODB3}:27017",
+                "host": "mongo3:27022",
                 "priority": 0
             }
         ]
